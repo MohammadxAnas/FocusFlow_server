72765dbfe078e389f83a05bf04f28d7db351a717
MohammadxAnas <mohammadanasqadar@gmail.com>
2025-03-25 22:39:45 +0530

    fix

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,14 +1,14 @@
 const {Router} = require("express");
 const { signupValidation, loginValidation, codeValidation } = require("../middleware/authvalidation");
 const { signup, login, confirmEmail, removeAccount, logout } = require("../controller/authcontroller");
-const ensureAuthenticated = require("../middleware/auth");
+const validateSession = require("../middleware/validateSession");
 
 const router = Router();
 
 router.post("/login",loginValidation,login);
 router.post("/signup",signupValidation,signup);
 router.post("/verify",codeValidation,confirmEmail);
-router.delete("/remove/:userId",ensureAuthenticated,removeAccount);
+router.delete("/remove/:userId", validateSession,removeAccount);
 router.post("/logout/:userId",logout);
 
 
